5c895e0d8570626c5732ad2807fd186821117e74
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-06-15 15:24:34 +0300

    fix: adjust the apt install command at the beginning
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo su
-apt update -y git open-vm-tools open-vm-tools-desktop
+apt update
+apt install -y git open-vm-tools open-vm-tools-desktop
 git clone https://github.com/iypetrov/dev-config.git /root/projects/common/dev-config
 cd /root/projects/common/dev-config
 git remote set-url origin git@github.com:iypetrov/dev-config.git
