72670e99a9de50a6dfd2ed91b23ca5412b58b7ce
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-06-15 23:52:24 +0300

    fix: typo
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- scripts/asdf.sh
+++ scripts/asdf.sh
@@ -7,8 +7,8 @@ fi
 . "/root/.asdf/asdf.sh"
 . "/root/.asdf/completions/asdf.bash"
 while read line; do
-    plugin="$(echo "${line}" | cur -d ' ' -f 1)"
-    version="$(echo "${line}" | cur -d ' ' -f 2)"
+    plugin="$(echo "${line}" | cut -d ' ' -f 1)"
+    version="$(echo "${line}" | cut -d ' ' -f 2)"
     url="$(asdf plugin list all | grep -E "^${plugin}[[:space:]]+" | xargs | cut -d ' ' -f 2)"
     asdf plugin add "${plugin}" "${url}"
     asdf install "${plugin}" "${version}"
