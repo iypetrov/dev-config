ce606984446c6744c665f7ea51288740e88d2390
Ilia Petrov <72142537+iypetrov@users.noreply.github.com>
2025-06-15 23:45:07 +0300

    fix: asdf

--- scripts/asdf.sh
+++ scripts/asdf.sh
@@ -2,15 +2,14 @@
 
 if [[ ! -d "/root/.asdf" ]]; then
     git clone https://github.com/asdf-vm/asdf.git /root/.asdf --branch v0.11.0
-    . "/root/.asdf/asdf.sh"
-    . "/root/.asdf/completions/asdf.bash"
 fi
 
-while read plugin; do
+. "/root/.asdf/asdf.sh"
+. "/root/.asdf/completions/asdf.bash"
+while read line; do
+    plugin="$(echo "${line}" | cur -d ' ' -f 1)"
+    version="$(echo "${line}" | cur -d ' ' -f 2)"
     url="$(asdf plugin list all | grep -E "^${plugin}[[:space:]]+" | xargs | cut -d ' ' -f 2)"
-    echo "${plugin}"
-    echo "${url}"
     asdf plugin add "${plugin}" "${url}"
+    asdf install "${plugin}" "${version}"
 done < <(cat /root/.tool-versions | cut -d ' ' -f 1)
-
-asdf install
