1841681c8037ea89c59554166a42daa656e09999
Ilia Petrov <72142537+iypetrov@users.noreply.github.com>
2025-07-01 08:08:16 +0300

    chore: cp i3 config

--- scripts/dotfiles-setup.sh
+++ scripts/dotfiles-setup.sh
@@ -4,19 +4,19 @@ curr_prj="$(pwd)"
 prj_dir="/root/projects"
 
 do_dotfiles_setup() {
-  	read -srp "GitHub Personal Access Token: " GH_PAT
-  	echo
-   	rm -rf /root/.bashrc
-    rm -rf /root/.gitconfig
-   	rm -rf /home/ipetrov/.bashrc
-	git clone https://github.com/tmux-plugins/tpm /root/.tmux/plugins/tpm
-	git clone https://iypetrov:${GH_PAT}@github.com/iypetrov/.dotfiles.git "${prj_dir}/common/.dotfiles"
-  	cd "${prj_dir}/common"
-	stow --target=/root .dotfiles
-    sudo -u ipetrov stow --target=/home/ipetrov .dotfiles
- 	cd "${prj_dir}/common/.dotfiles"
-  	git remote set-url origin git@github.com:iypetrov/.dotfiles.git
- 	cd "${curr_prj}"
+  read -srp "GitHub Personal Access Token: " GH_PAT
+  echo
+  rm -rf /root/.bashrc
+  rm -rf /root/.gitconfig
+  git clone https://github.com/tmux-plugins/tpm /root/.tmux/plugins/tpm
+  git clone https://iypetrov:${GH_PAT}@github.com/iypetrov/.dotfiles.git "${prj_dir}/common/.dotfiles"
+  cd "${prj_dir}/common"
+  stow --target=/root .dotfiles
+  mkdir -p "/home/ipetrov/.config/i3"
+  cp "/root/projects/common/.dotfiles/.config/i3/config" "/home/ipetrov/.config/i3/config"
+  cd "${prj_dir}/common/.dotfiles"
+  git remote set-url origin git@github.com:iypetrov/.dotfiles.git
+  cd "${curr_prj}"
 }
 
 if [[ -d "${prj_dir}/common/.dotfiles" ]]; then
